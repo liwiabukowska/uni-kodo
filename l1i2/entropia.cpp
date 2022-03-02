ede5bddb15407902f9920a4a888d3e673a17effc
Liwia Bukowska <44477673+liwiabukowska@users.noreply.github.com>
2022-03-02 03:29:26 +0100

    chyba tak zostawiam

--- l1i2/entropia.cpp
+++ l1i2/entropia.cpp
@@ -34,23 +34,22 @@ auto read(char* name)
 
 namespace logic {
 
-void run(std::vector<char> vec)
+void entropia_warunkowa(const std::vector<char>& vec)
 {
-    auto space_size = vec.size();
+    auto const space_size = vec.size();
 
     double h_y_x = 0;
-    for (uint32_t j = 0; j <= 0xff; ++j) {
-        char x = j;
+    for (uint32_t x = 0; x < 0x100; ++x) {
 
         // amount_by_y[y] zawiera ilosc y wystepujacych po x
         // obliczam rownolegle bo tak jest duzo szybciej (z 13s ma 40ms)
         // poniewaz tak nie przelatuje przez pamiec 256 razy
         // oraz gwarantuje ze dokladnie jeden y zostanie dodany jezeli tylko zgadza sie poprzedni x
         // i nie trzeba opuszczac obliczen kiedy nie zgadza sie y (a potem wykonywac podobne jeszcze raz)
-        std::array<uint64_t, 256> amount_y_if_x { 0 };
-        char last = 0;
+        std::array<uint64_t, 0x100> amount_y_if_x { 0 };
+        unsigned char last = 0;
         for (size_t i = 0; i < space_size; ++i) {
-            char current = vec[i];
+            unsigned char current = vec[i];
 
             if (last == x) {
                 ++amount_y_if_x[current];
@@ -62,7 +61,9 @@ void run(std::vector<char> vec)
         double h_y_if_x = 0;
         for (uint32_t y = 0; y <= 0xff; ++y) {
             if (amount_y_if_x[y]) {
-                h_y_if_x += (double)amount_y_if_x[y] * std::log2((double)space_size / amount_y_if_x[y]) / space_size;
+                auto partial_h_y_if_x = (double)amount_y_if_x[y] * std::log2((double)space_size / amount_y_if_x[y]) / space_size;
+                // std::cout << partial_h_y_if_x << "\n";
+                h_y_if_x += partial_h_y_if_x;
             }
         }
         std::cout << "H(Y|"
@@ -82,6 +83,34 @@ void run(std::vector<char> vec)
     }
     std::cout << "H(Y|X) = " << h_y_x << "\n";
 }
+
+void entropia(const std::vector<char>& vec)
+{
+    auto const space_size = vec.size();
+
+    double h_x;
+    for (uint32_t x = 0; x < 0x100; ++x) {
+        uint64_t amount_x = 0;
+        for (size_t i = 0; i < space_size; ++i) {
+            if (vec[i] == x) {
+                ++amount_x;
+            }
+        }
+
+        if (amount_x != 0) {
+            auto partial_h = (double)amount_x * std::log2(space_size / amount_x) / space_size;
+            // std::cout << partial_h << "\n";
+            h_x += partial_h;
+        }
+    }
+    std::cout << "H(X) = " << h_x << "\n";
+}
+
+void run(const std::vector<char>& vec)
+{
+    entropia_warunkowa(vec);
+    entropia(vec);
+}
 }
 
 int main(int argc, char** argv)
@@ -96,9 +125,11 @@ int main(int argc, char** argv)
     utils::time_it<std::chrono::milliseconds> timer {};
 
     auto data = files::read(argv[1]);
-    std::cout << "<info>: dlugosc: " << data.size() << "\n";
+    if (data.size() != 0) {
+        std::cout << "<info>: dlugosc: " << data.size() << "\n";
 
-    logic::run(data);
+        logic::run(data);
+    }
 
     std::cout << "<info>: program wykonal sie w " << timer.measure() << "ms";
 }
