98ebcb85dbe2b8afb44297a0a8a11e2d468cb92f
Liwia Bukowska <44477673+liwiabukowska@users.noreply.github.com>
2022-05-28 19:06:08 +0200

    coder juz jest zaraz zobacze czy sprawny1

--- l11i12/src/coder.cpp
+++ l11i12/src/coder.cpp
@@ -1,4 +1,5 @@
 #include "coding/graphics_statistics.hpp"
+#include "coding/jpg_coders.hpp"
 #include "coding/quantization.hpp"
 #include "coding/statistics.hpp"
 // #include "jpg_coders.hpp"
@@ -22,9 +23,10 @@
 #include <vector>
 
 struct options {
+    std::string program_mode {};
     std::string tga_file_path {};
     std::string tga_save_file_path {};
-    std::string opt_mode {};
+    std::string quant_mode {};
 
     std::string r_quant {};
     std::string g_quant {};
@@ -55,17 +57,21 @@ auto create_chooser(const options& opts) -> quant_chooser
         ss >> bits;
     }
 
-    if (opts.opt_mode == "mse") {
+    if (opts.quant_mode == "mse") {
         return quant_choosers::mse { bits };
-    } else if (opts.opt_mode == "snr") {
+    } else if (opts.quant_mode == "snr") {
         return quant_choosers::snr { bits };
-    } else if (opts.opt_mode == "manual") {
+    } else if (opts.quant_mode == "manual_rgb") {
         return [q]([[maybe_unused]] std::vector<uint8_t> const& rgb) -> quants {
             return q;
         };
+    } else if (opts.quant_mode == "manual") {
+        return [bits]([[maybe_unused]] std::vector<uint8_t> const& rgb) -> quants {
+            return { bits, bits, bits };
+        };
     }
 
-    throw std::runtime_error { "nie tryb dzialania=" + opts.opt_mode };
+    throw std::runtime_error { "nie tryb dzialania=" + opts.quant_mode };
 }
 
 auto run_on_file(const options& opts)
@@ -92,6 +98,9 @@ auto run_on_file(const options& opts)
 
     auto quant_chooser = create_chooser(opts);
     quants q = quant_chooser(image._data);
+    {
+        std::cout << "wybrany kwantyzator=" << q << std::endl;
+    }
 
     auto [r_vals, g_vals, b_vals] = tga::split_channels(image._data);
 
@@ -101,17 +110,17 @@ auto run_on_file(const options& opts)
 
     std::vector<uint8_t> rgb_vals_quantized = tga::join_channels(r_vals_quantized, g_vals_quantized, b_vals_quantized);
 
-    tga::accessor_RGB accessor { image._data, image._width, image._height };
-    tga::accessor_MONO accessor_r { r_vals, image._width, image._height };
-    tga::accessor_MONO accessor_g { g_vals, image._width, image._height };
-    tga::accessor_MONO accessor_b { b_vals, image._width, image._height };
-
-    tga::accessor_RGB accessor_quantized { rgb_vals_quantized, image._width, image._height };
-    tga::accessor_MONO accessor_r_quantized { r_vals_quantized, image._width, image._height };
-    tga::accessor_MONO accessor_g_quantized { g_vals_quantized, image._width, image._height };
-    tga::accessor_MONO accessor_b_quantized { b_vals_quantized, image._width, image._height };
-
     {
+        tga::accessor_RGB accessor { image._data, image._width, image._height };
+        tga::accessor_MONO accessor_r { r_vals, image._width, image._height };
+        tga::accessor_MONO accessor_g { g_vals, image._width, image._height };
+        tga::accessor_MONO accessor_b { b_vals, image._width, image._height };
+
+        tga::accessor_RGB accessor_quantized { rgb_vals_quantized, image._width, image._height };
+        tga::accessor_MONO accessor_r_quantized { r_vals_quantized, image._width, image._height };
+        tga::accessor_MONO accessor_g_quantized { g_vals_quantized, image._width, image._height };
+        tga::accessor_MONO accessor_b_quantized { b_vals_quantized, image._width, image._height };
+
         auto mse = statistics::mse(accessor._image, accessor_quantized._image);
         auto mse_r = statistics::mse(accessor_r._image, accessor_r_quantized._image);
         auto mse_g = statistics::mse(accessor_g._image, accessor_g_quantized._image);
@@ -141,8 +150,6 @@ auto run_on_file(const options& opts)
         std::cout << "entropia pliku wyjsciowego (B)=" << statistics::entropy(accessor_b_quantized._image) << std::endl;
         std::cout << "entropia pliku wyjsciowego (R)=" << statistics::entropy(accessor_r_quantized._image) << std::endl;
         std::cout << "entropia pliku wyjsciowego (G)=" << statistics::entropy(accessor_g_quantized._image) << std::endl;
-
-        std::cout << "wybrany kwantyzator=" << q << std::endl;
     }
 
     std::ofstream save_file { opts.tga_save_file_path };
@@ -158,6 +165,70 @@ auto run_on_file(const options& opts)
     save_file << save_data;
 }
 
+auto encode(std::vector<uint8_t> const& input_data, quants q) -> std::vector<uint8_t>
+{
+    using namespace coding;
+
+    tga::image image {};
+    image.from_binary(input_data);
+    std::cout << image._header << std::endl;
+
+    if (image._image_format != tga::image_format::RGB) {
+        throw std::runtime_error { "obrazek nie jest w formacie rgb" };
+    }
+
+    auto [r_vals, g_vals, b_vals] = tga::split_channels(image._data);
+
+    std::vector<uint8_t> r_vals_quantized = uniform_quantization(r_vals, q.r_quant);
+    std::vector<uint8_t> g_vals_quantized = uniform_quantization(g_vals, q.g_quant);
+    std::vector<uint8_t> b_vals_quantized = uniform_quantization(b_vals, q.b_quant);
+
+    tga::accessor_MONO quantized_accessor_r { r_vals_quantized, image._width, image._height };
+    tga::accessor_MONO quantized_accessor_g { g_vals_quantized, image._width, image._height };
+    tga::accessor_MONO quantized_accessor_b { b_vals_quantized, image._width, image._height };
+
+    std::vector<uint8_t> r_vals_diff = differential_coding::encode<jpg_predictors::predictor_new>(quantized_accessor_r);
+    std::vector<uint8_t> g_vals_diff = differential_coding::encode<jpg_predictors::predictor_new>(quantized_accessor_g);
+    std::vector<uint8_t> b_vals_diff = differential_coding::encode<jpg_predictors::predictor_new>(quantized_accessor_b);
+
+    std::vector<uint8_t> rgb_vals_diff = tga::join_channels(r_vals_diff, g_vals_diff, b_vals_diff);
+
+    tga::image save_image = image;
+    save_image._data = rgb_vals_diff; // FIXME niepotrzebna kopia
+
+    return save_image.to_binary();
+}
+
+auto decode(std::vector<uint8_t> const& input_data, [[maybe_unused]] quants q) -> std::vector<uint8_t>
+{
+    using namespace coding;
+
+    tga::image image {};
+    image.from_binary(input_data);
+    std::cout << image._header << std::endl;
+
+    if (image._image_format != tga::image_format::RGB) {
+        throw std::runtime_error { "obrazek nie jest w formacie rgb" };
+    }
+
+    auto [r_vals_diff, g_vals_diff, b_vals_diff] = tga::split_channels(image._data);
+
+    tga::accessor_MONO quantized_accessor_r { r_vals_diff, image._width, image._height };
+    tga::accessor_MONO quantized_accessor_g { g_vals_diff, image._width, image._height };
+    tga::accessor_MONO quantized_accessor_b { b_vals_diff, image._width, image._height };
+
+    std::vector<uint8_t> r_vals_quantized = differential_coding::decode<jpg_predictors::predictor_new>(quantized_accessor_r);
+    std::vector<uint8_t> g_vals_quantized = differential_coding::decode<jpg_predictors::predictor_new>(quantized_accessor_g);
+    std::vector<uint8_t> b_vals_quantized = differential_coding::decode<jpg_predictors::predictor_new>(quantized_accessor_b);
+
+    std::vector<uint8_t> rgb_vals_quantized = tga::join_channels(r_vals_quantized, g_vals_quantized, b_vals_quantized);
+
+    tga::image save_image = image;
+    save_image._data = rgb_vals_quantized; // FIXME niepotrzebna kopia
+
+    return save_image.to_binary();
+}
+
 int main(int argc, char** argv)
 {
     std::ios_base::sync_with_stdio(false);
@@ -165,13 +236,14 @@ int main(int argc, char** argv)
     utils::args_helper parser {
         "kodowanie rownomierne\n"
         "\n"
-        "uzycie: program <plik tga> <zapis pliku tga> <mse/snr/manual> [jezeli manual to -r val -g val -b val] [-h help]\n"
+        "uzycie: program <encode/decode> <wejsciowy plik tga> <wyjsciowy pliku tga> <mse/snr/manual/manual_rgb> [jezeli manual_rgb to -r val -g val -b val] [-h help]\n"
     };
     options opts {};
 
+    parser.set_positional({ .write_to = opts.program_mode });
     parser.set_positional({ .write_to = opts.tga_file_path });
     parser.set_positional({ .write_to = opts.tga_save_file_path });
-    parser.set_positional({ .write_to = opts.opt_mode });
+    parser.set_positional({ .write_to = opts.quant_mode });
 
     parser.set_optional({ .write_to = opts.r_quant, .symbol = "-r" });
     parser.set_optional({ .write_to = opts.g_quant, .symbol = "-g" });
@@ -179,16 +251,56 @@ int main(int argc, char** argv)
 
     parser.set_optional({ .write_to = opts.bits, .symbol = "--bits" });
 
-    if (!parser.parse(argc, argv)) {
+    bool help {};
+    parser.set_boolean({ .write_to = help, .symbol = "-h" });
+    parser.set_boolean({ .write_to = help, .symbol = "--help" });
+
+    if (!parser.parse(argc, argv) || help) {
         std::cout << parser.help_page();
         return 0;
     }
 
-    utils::time_it<std::chrono::milliseconds> timer {};
+    using namespace coding;
+    using utils::vector_streams::binary::operator<<;
+    using utils::vector_streams::binary::operator>>;
 
+    std::vector<unsigned char> input_data {};
+    std::ifstream input_file { opts.tga_file_path };
+    if (!input_file) {
+        throw std::runtime_error { "nie mozna otworzyc pliku=" + opts.tga_file_path };
+    }
+
+    input_file >> input_data;
+    std::vector<uint8_t> output_data {};
+
+    utils::time_it<std::chrono::milliseconds> timer {};
     timer.set();
-    run_on_file(opts);
+
+    quants q {};
+    {
+        tga::image image {};
+        image.from_binary(input_data);
+
+        auto quant_chooser = create_chooser(opts);
+        q = quant_chooser(image._data);
+        std::cout << "wybrany kwantyzator=" << q << std::endl;
+    }
+
+    if (opts.program_mode == "encode") {
+        output_data = encode(input_data, q);
+    } else if (opts.program_mode == "decode") {
+        output_data = decode(input_data, q);
+    } else {
+        throw std::runtime_error { "nie znany sposob dzialania programu!" + opts.program_mode + " podaj 'encode' lub 'decode'" };
+    }
     uint64_t time = timer.measure();
+
+    std::ofstream output_file { opts.tga_save_file_path };
+    if (!output_file) {
+        throw std::runtime_error { "nie mozna otworzyc pliku=" + opts.tga_save_file_path };
+    }
+
+    output_file << output_data;
 
     std::cout << "czas dzialania=" << time << "ms\n";
 }
--- l11i12/src/coding/jpg_coders.hpp
+++ l11i12/src/coding/jpg_coders.hpp
@@ -6,118 +6,147 @@
 #include <cstdint>
 #include <tuple>
 
+namespace coding {
+
 namespace jpg_predictors {
-struct predictor_1 {
-    static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
-    {
-        uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
-        // uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
-        // uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
+    struct predictor_1 {
+        static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
+        {
+            uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
+            // uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
+            // uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
 
-        return w;
-    }
-};
-
-struct predictor_2 {
-    static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
-    {
-        // uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
-        uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
-        // uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
-
-        return n;
-    }
-};
-
-struct predictor_3 {
-    static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
-    {
-        // uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
-        // uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
-        uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
-
-        return nw;
-    }
-};
-
-struct predictor_4 {
-    static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
-    {
-        uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
-        uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
-        uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
-
-        return n + w - nw;
-    }
-};
-
-struct predictor_5 {
-    static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
-    {
-        uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
-        uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
-        uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
-
-        return n + (w - nw) / 2;
-    }
-};
-
-struct predictor_6 {
-    static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
-    {
-        uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
-        uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
-        uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
-
-        return w + (n - nw) / 2;
-    }
-};
-
-struct predictor_7 {
-    static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
-    {
-        uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
-        uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
-        // uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
-
-        return (n + w) / 2;
-    }
-};
-
-struct predictor_new {
-    static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
-    {
-        uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
-        uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
-        uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
-
-        if (nw >= std::max(w, n)) {
-            return std::max(w, n);
-        } else if (nw <= std::min(w, n)) {
-            return std::min(w, n);
+            return w;
         }
+    };
 
-        return w + n - nw;
-    }
-};
+    struct predictor_2 {
+        static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
+        {
+            // uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
+            uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
+            // uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
 
-template <typename Predictor>
-auto encode(tga::accessor_MONO const& acc) -> std::vector<uint8_t>
-{
-    std::vector<uint8_t> vals;
-    vals.resize(acc.size());
-
-    for (size_t y {}; y < acc._height; ++y) {
-        for (size_t x {}; x < acc._width; ++x) {
-            auto nth = acc.nth(x, y);
-            uint8_t predicted = Predictor::predict(acc, x, y);
-            uint8_t real = acc[nth];
-
-            uint8_t diff = real - predicted;
-            vals[nth] = diff;
+            return n;
         }
-    }
+    };
 
-    return vals;
+    struct predictor_3 {
+        static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
+        {
+            // uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
+            // uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
+            uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
+
+            return nw;
+        }
+    };
+
+    struct predictor_4 {
+        static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
+        {
+            uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
+            uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
+            uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
+
+            return n + w - nw;
+        }
+    };
+
+    struct predictor_5 {
+        static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
+        {
+            uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
+            uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
+            uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
+
+            return n + (w - nw) / 2;
+        }
+    };
+
+    struct predictor_6 {
+        static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
+        {
+            uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
+            uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
+            uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
+
+            return w + (n - nw) / 2;
+        }
+    };
+
+    struct predictor_7 {
+        static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
+        {
+            uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
+            uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
+            // uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
+
+            return (n + w) / 2;
+        }
+    };
+
+    struct predictor_new {
+        static auto predict(tga::accessor_MONO const& acc, uint32_t x, uint32_t y) -> uint8_t
+        {
+            uint8_t w = acc.check_range(x - 1, y) ? acc[acc.nth(x - 1, y)] : 0;
+            uint8_t n = acc.check_range(x, y + 1) ? acc[acc.nth(x, y + 1)] : 0;
+            uint8_t nw = acc.check_range(x - 1, y + 1) ? acc[acc.nth(x - 1, y + 1)] : 0;
+
+            if (nw >= std::max(w, n)) {
+                return std::max(w, n);
+            } else if (nw <= std::min(w, n)) {
+                return std::min(w, n);
+            }
+
+            return w + n - nw;
+        }
+    };
+
 }
+
+namespace differential_coding {
+
+    template <typename Predictor>
+    auto encode(tga::accessor_MONO const& acc) -> std::vector<uint8_t>
+    {
+        std::vector<uint8_t> vals;
+        vals.resize(acc.size());
+
+        for (size_t y {}; y < acc._height; ++y) {
+            for (size_t x {}; x < acc._width; ++x) {
+                auto nth = acc.nth(x, y);
+                uint8_t predicted = Predictor::predict(acc, x, y);
+                uint8_t real = acc[nth];
+
+                uint8_t diff = real - predicted;
+                vals[nth] = diff;
+            }
+        }
+
+        return vals;
+    }
+
+    template <typename Predictor>
+    auto decode(tga::accessor_MONO const& acc) -> std::vector<uint8_t>
+    {
+        std::vector<uint8_t> vals;
+        vals.resize(acc.size());
+
+        for (size_t y {}; y < acc._height; ++y) {
+            for (size_t x {}; x < acc._width; ++x) {
+                auto nth = acc.nth(x, y);
+                uint8_t predicted = Predictor::predict(acc, x, y);
+                uint8_t diff = acc[nth];
+
+                uint8_t real = predicted + diff;
+                vals[nth] = real;
+            }
+        }
+
+        return vals;
+    }
+
+}
+
 }
